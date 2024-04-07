e6dca1c6640528fc887358ba15df249aaee0c558
ajay15codes <dawandey15aj@gmail.com>
2024-04-07 14:14:30 +0530

    Push for merge

--- QuizProdigy-Angular/src/app/components/modules/teacher/components/set-exam/set-exam.component.ts
+++ QuizProdigy-Angular/src/app/components/modules/teacher/components/set-exam/set-exam.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { SetExamService } from '../../../../../services/setExam/set-exam.service';
 
@@ -7,89 +7,45 @@ import { SetExamService } from '../../../../../services/setExam/set-exam.service
   templateUrl: './set-exam.component.html',
   styleUrl: './set-exam.component.css',
 })
-export class SetExamComponent {
-  constructor(private setExamService: SetExamService,private formBuilder: FormBuilder) {}
+export class SetExamComponent implements OnInit {
+  examDetailsForm!: FormGroup;
+  questionDetailsForm!: FormGroup;
+  questions: any[] = [];
 
-  examDetails = new FormGroup({
-    examName: new FormControl(''),
-    totalQuestions: new FormControl(''),
-    examDuration: new FormControl(''),
-    timeAllotedPerQuestion: new FormControl(''),
-    handsOn: new FormControl(''),
-  });
+  constructor(private formBuilder: FormBuilder) {}
 
-
-// Step 2-> Set Question Paper
-
-
-// loginForm!: FormGroup<any>;
-  
-
-  questionDetails!: FormGroup;
-questions: any[] = []; // Initialize as per your requirement
-answers: any[][] = []; // Initialize as per your requirement
-
-ngOnInit(): void {
-  this.initializeForm();
-}
-
-initializeForm() {
-  this.questionDetails = this.formBuilder.group({
-    // Initialize your form controls here dynamically based on questions and answers
-  });
-
-  // Example: If you want to initialize form controls dynamically based on questions and answers
-  this.questions.forEach((question, qIndex) => {
-    const questionControlName = 'question' + qIndex;
-    this.questionDetails.addControl(questionControlName, this.formBuilder.control('', Validators.required));
-
-    this.answers[qIndex].forEach((answer, aIndex) => {
-      const answerControlName = 'answer' + qIndex + aIndex;
-      this.questionDetails.addControl(answerControlName, this.formBuilder.control('', Validators.required));
+  ngOnInit(): void {
+    this.examDetailsForm = this.formBuilder.group({
+      examName: ['', Validators.required],
+      totalQuestions: ['', Validators.required],
+      examDuration: ['', Validators.required],
+      timeAllotedPerQuestion: ['', Validators.required],
+      handsOn: ['false', Validators.required]
     });
 
-    // Add answer control for each question
-    const answerControlName = 'answer' + qIndex;
-    this.questionDetails.addControl(answerControlName, this.formBuilder.control('', Validators.required));
-  });
-}
+    this.questionDetailsForm = this.formBuilder.group({
+      questions: this.formBuilder.array([])
+    });
+  }
 
-addAnswerField(qIndex: number) {
-  // Assuming answers is an array of arrays
-  this.answers[qIndex].push(''); // Add an empty string as a new answer
-}
+  addQuestion() {
+    const questionFormGroup = this.formBuilder.group({
+      question: ['', Validators.required],
+      options: this.formBuilder.array([
+        this.formBuilder.control('', Validators.required),
+        this.formBuilder.control('', Validators.required),
+        this.formBuilder.control('', Validators.required),
+        this.formBuilder.control('', Validators.required)
+      ]),
+      correctAnswer: ['', Validators.required]
+    });
 
-addQuestion() {
-  this.questions.push(''); // Add an empty string as a new question
-  this.answers.push([]); // Add an empty array for answers corresponding to the new question
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // questionDetails = FormGroup({
-
-  // })
+    this.questions.push(questionFormGroup);
+  }
 
   setExamDetails() {
-    this.setExamService.setExamDetails(this.examDetails).subscribe(
-      (data: any) => {
-        console.log('Exam Details are :', data);
-      },
-      (error: any) => {
-        console.log('Error sending Exam Details :', error);
-      }
-    );
+    // Implement method to set exam details
+    console.log('Exam Details:', this.examDetailsForm.value);
+    console.log('Question Details:', this.questions.map(q => q.value));
   }
 }
